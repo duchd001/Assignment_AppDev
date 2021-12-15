9cb175193d87221b3a5b6e42d002b3035ff229b4
duchd001 <85073097+duchd001@users.noreply.github.com>
2021-12-15 22:59:32 +0700

    Create Trainer Model and migration AddTrainer

---
+++ Assignment_AppDev/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Assignment_AppDev.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Trainer> Trainers { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Assignment_AppDev/Models/Trainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_AppDev.Models
+{
+    public class Trainer
+    {
+		public int ID { get; set; }
+
+		[DisplayName("Trainer ID")]
+		[Required]
+		public string TrainerID { get; set; }
+
+		[DisplayName("Trainer Name")]
+		public string Name { get; set; }
+		public int Phone { get; set; }
+	}
+}
