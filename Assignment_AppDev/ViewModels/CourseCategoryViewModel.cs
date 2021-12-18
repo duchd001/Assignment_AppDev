5c5f82e029982245ebe9f6e2b72fbe1d62a0f112
Dat <phungdat020501@gmail.com>
2021-12-18 19:39:39 +0700

    update CourseCategoryViewModel

---
+++ Assignment_AppDev/ViewModels/CourseCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Assignment_AppDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_AppDev.ViewModels
+{
+    public class CourseCategoryViewModel
+    {
+        public Course Course { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
